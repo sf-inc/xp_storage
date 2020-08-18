5804529edb3ae40e89a50c05ac8ba10c27ddbcc0
Charly <charly5bollinger@gmail.com>
2020-08-18 21:39:52 +0200

    Initial commit: empty fabric mod

---
+++ README.md
@@ -0,0 +1,5 @@
+# XP Storage
+
+A mod that lets you store your experience into an item!
+
+Port of XPTome (based on XPBook) to Fabric with a few homemade changes. Note that this mod is fully rewritten.
---
+++ src/main/java/com/github/charlyb01/xpstorage/Xpstorage.java
@@ -0,0 +1,10 @@
+package com.github.charlyb01.xpstorage;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Xpstorage implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
