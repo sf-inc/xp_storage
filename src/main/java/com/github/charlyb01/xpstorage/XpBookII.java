015a267d090386053d1993e8c1584411de997cd6
Charly <charly5bollinger@gmail.com>
2020-09-03 14:17:43 +0200

    Better book level makes the item more rare and fireproof (since it's upgraded with netherite ingot, it makes sense)

--- src/main/java/com/github/charlyb01/xpstorage/XpBookII.java
+++ src/main/java/com/github/charlyb01/xpstorage/XpBookII.java
@@ -1,11 +1,13 @@
 package com.github.charlyb01.xpstorage;
 
 
+import net.minecraft.util.Rarity;
+
 public class XpBookII extends XpBook {
     private final static int maxExperience = 3334;    // First 42 lvl
 
     public XpBookII() {
-        super(new Settings().maxDamage(maxExperience));
+        super(new Settings().maxDamage(maxExperience).rarity(Rarity.UNCOMMON).fireproof());
     }
 
     @Override
