f9c432b7177e738c382f5af57c66c26ebf43e408
Charly <charly5bollinger@gmail.com>
2020-09-04 16:33:18 +0200

    Stores the level variable so that the experience amount is automatically calculated

--- src/main/java/com/github/charlyb01/xpstorage/XpBookI.java
+++ src/main/java/com/github/charlyb01/xpstorage/XpBookI.java
@@ -2,7 +2,8 @@ package com.github.charlyb01.xpstorage;
 
 
 public class XpBookI extends XpBook {
-    private final static int maxExperience = 1395;    // First 30 lvl
+    private final static int maxLevel = 30;
+    private final static int maxExperience = Utils.getExperienceToLevel(maxLevel);
 
     public XpBookI() {
         super(new Settings().maxDamage(maxExperience));
--- src/main/java/com/github/charlyb01/xpstorage/XpBookIII.java
+++ src/main/java/com/github/charlyb01/xpstorage/XpBookIII.java
@@ -4,7 +4,8 @@ package com.github.charlyb01.xpstorage;
 import net.minecraft.util.Rarity;
 
 public class XpBookIII extends XpBook {
-    private final static int maxExperience = 12433;    // First 69 lvl
+    private final static int maxLevel = 69;
+    private final static int maxExperience = Utils.getExperienceToLevel(maxLevel);
 
     public XpBookIII() {
         super(new Settings().maxDamage(maxExperience).rarity(Rarity.RARE).fireproof());
