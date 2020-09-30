ce0b759a7b291baa8d7c5cfb8d2f94a879c82fdb
Charly <charly5bollinger@gmail.com>
2020-09-30 22:15:34 +0200

    Rename XP Book I
    Renamed the XP Book 1 id to xp_book as it was before to ensure compatibility with world using old mod version

--- src/main/java/com/github/charlyb01/xpstorage/Xpstorage.java
+++ src/main/java/com/github/charlyb01/xpstorage/Xpstorage.java
@@ -6,13 +6,13 @@ import net.minecraft.util.registry.Registry;
 
 public class Xpstorage implements ModInitializer {
 
-    public static final XpBookI xp_book1 = new XpBookI();
+    public static final XpBookI xp_book = new XpBookI();
     public static final XpBookII xp_book2 = new XpBookII();
     public static final XpBookIII xp_book3 = new XpBookIII();
 
     @Override
     public void onInitialize() {
-        Registry.register(Registry.ITEM, new Identifier("xp_storage", "xp_book1"), xp_book1);
+        Registry.register(Registry.ITEM, new Identifier("xp_storage", "xp_book"), xp_book);
         Registry.register(Registry.ITEM, new Identifier("xp_storage", "xp_book2"), xp_book2);
         Registry.register(Registry.ITEM, new Identifier("xp_storage", "xp_book3"), xp_book3);
     }
