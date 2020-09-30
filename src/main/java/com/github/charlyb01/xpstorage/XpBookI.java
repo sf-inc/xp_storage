f036bdf6b858dcab271d7161f527a3bfbf826e28
Charly <charly5bollinger@gmail.com>
2020-09-30 22:15:34 +0200

    Rename XP Book I
    Renamed the XP Book 1 id to xp_book as it was before to ensure compatibility with world using old mod version

--- src/main/java/com/github/charlyb01/xpstorage/XpBookI.java
+++ src/main/java/com/github/charlyb01/xpstorage/XpBookI.java
@@ -1,10 +1,6 @@
 package com.github.charlyb01.xpstorage;
 
 
-import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.item.ItemStack;
-import net.minecraft.world.World;
-
 public class XpBookI extends XpBook {
     private final static int maxLevel = 30;
     private final static int maxExperience = Utils.getExperienceToLevel(maxLevel);
