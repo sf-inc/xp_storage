19cb7036cd0e626e70123612b4c0b999e6ee77ff
Charly <charly5bollinger@gmail.com>
2020-11-30 19:04:56 +0100

    Add amount to xp bottle
    Mixin the xp bottle (item and entity) so that if a value is set for a xp bottle, it will create this amount of xp when thrown

---
+++ src/main/java/com/github/charlyb01/xpstorage/mixin/XpBottleEntityMixin.java
@@ -0,0 +1,50 @@
+package com.github.charlyb01.xpstorage.mixin;
+
+import com.github.charlyb01.xpstorage.imixin.XpBottleEntityIMixin;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.ExperienceOrbEntity;
+import net.minecraft.entity.projectile.thrown.ExperienceBottleEntity;
+import net.minecraft.entity.projectile.thrown.ThrownItemEntity;
+import net.minecraft.potion.PotionUtil;
+import net.minecraft.potion.Potions;
+import net.minecraft.util.hit.HitResult;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(ExperienceBottleEntity.class)
+public abstract class XpBottleEntityMixin extends ThrownItemEntity implements XpBottleEntityIMixin {
+    @Unique
+    int xpAmount = 0;
+
+    public XpBottleEntityMixin(EntityType<? extends ThrownItemEntity> entityType, World world) {
+        super(entityType, world);
+    }
+
+    @Override
+    public void setAmount(final int xpAmount) {
+        this.xpAmount = xpAmount;
+    }
+
+    @Inject(method = "onCollision", at = @At("HEAD"), cancellable = true)
+    private void changeXpAmount(HitResult hitResult, CallbackInfo ci) {
+        if (xpAmount > 0) {
+            if (!this.world.isClient) {
+            this.world.syncWorldEvent(2002, this.getBlockPos(), PotionUtil.getColor(Potions.WATER));
+            int i = xpAmount;
+
+            while(i > 0) {
+            int j = ExperienceOrbEntity.roundToOrbSize(i);
+            i -= j;
+            this.world.spawnEntity(new ExperienceOrbEntity(this.world, this.getX(), this.getY(), this.getZ(), j));
+            }
+
+            this.remove();
+            }
+            ci.cancel();
+        }
+    }
+}
