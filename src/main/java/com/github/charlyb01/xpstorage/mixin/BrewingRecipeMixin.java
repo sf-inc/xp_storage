30cc406f970ef8878e7314f7b02e7c3de25216fc
Charly <charly5bollinger@gmail.com>
2020-12-03 16:38:56 +0100

    add xpbook ingredient
    xpbook is now considered as an ingredient for brewing. it means it can now be placed in the brewing stand

--- src/main/java/com/github/charlyb01/xpstorage/mixin/BrewingRecipeMixin.java
+++ src/main/java/com/github/charlyb01/xpstorage/mixin/BrewingRecipeMixin.java
@@ -1,2 +1,19 @@
-package com.github.charlyb01.xpstorage.mixin;public class BrewingRecipeMixin {
+package com.github.charlyb01.xpstorage.mixin;
+
+import com.github.charlyb01.xpstorage.XpBook;
+import net.minecraft.item.ItemStack;
+import net.minecraft.recipe.BrewingRecipeRegistry;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(BrewingRecipeRegistry.class)
+public class BrewingRecipeMixin {
+    @Inject(method = "isValidIngredient", at = @At("HEAD"), cancellable = true)
+    private static void registerXpBookIngredient(ItemStack stack, CallbackInfoReturnable<Boolean> cir) {
+        if (stack.getItem() instanceof XpBook) {
+            cir.setReturnValue(true);
+        }
+    }
 }
