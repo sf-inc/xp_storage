a8653461547879f87c7679ea572f5bb155f0d364
Charly <charly5bollinger@gmail.com>
2020-12-08 19:45:55 +0100

    add xp amount to entity
    import the cc api entity and set up in my components

--- src/main/java/com/github/charlyb01/xpstorage/cardinal/MyComponents.java
+++ src/main/java/com/github/charlyb01/xpstorage/cardinal/MyComponents.java
@@ -2,14 +2,22 @@ package com.github.charlyb01.xpstorage.cardinal;
 
 import dev.onyxstudios.cca.api.v3.component.ComponentKey;
 import dev.onyxstudios.cca.api.v3.component.ComponentRegistry;
+import dev.onyxstudios.cca.api.v3.entity.EntityComponentFactoryRegistry;
+import dev.onyxstudios.cca.api.v3.entity.EntityComponentInitializer;
 import dev.onyxstudios.cca.api.v3.item.ItemComponentFactoryRegistry;
 import dev.onyxstudios.cca.api.v3.item.ItemComponentInitializer;
+import net.minecraft.entity.projectile.thrown.ExperienceBottleEntity;
 import net.minecraft.item.Items;
 import net.minecraft.util.Identifier;
 
-public final class MyComponents implements ItemComponentInitializer {
+public final class MyComponents implements EntityComponentInitializer, ItemComponentInitializer {
     public static final ComponentKey<RandIntComponent> XP_AMOUNT =
             ComponentRegistry.getOrCreate(new Identifier("xp_storage", "xp_amount"), RandIntComponent.class);
+
+    @Override
+    public void registerEntityComponentFactories(EntityComponentFactoryRegistry registry) {
+        registry.registerFor(ExperienceBottleEntity.class, XP_AMOUNT, entity -> new XpAmountComponent());
+    }
 
     @Override
     public void registerItemComponentFactories(ItemComponentFactoryRegistry registry) {
