1a6128686fd35df5fddfdb9444871fa80af70fb2
Charly <charly5bollinger@gmail.com>
2020-12-08 18:26:56 +0100

    add constants for xp bottles
    xp from book: the (max) xp amount we take from the book when brewing
    lower bound random: we put a percentage of the previous amount in the xp bottle, it's random and this is the lower bound
    upper bound random: same but upper bound

--- src/main/java/com/github/charlyb01/xpstorage/config/ConstantsConfig.java
+++ src/main/java/com/github/charlyb01/xpstorage/config/ConstantsConfig.java
@@ -5,4 +5,10 @@ public class ConstantsConfig {
     public static final int MAX_LEVEL_I = 30;
     public static final int MAX_LEVEL_II = 42;
     public static final int MAX_LEVEL_III = 69;
+
+    // Experience values for the new xp bottles
+    public static final int XP_FROM_BOOK = 100;
+    // The bounds are in percentages!
+    public static final int LOWER_BOUND_RANDOM = 85;
+    public static final int UPPER_BOUND_RANDOM = 130;
 }
