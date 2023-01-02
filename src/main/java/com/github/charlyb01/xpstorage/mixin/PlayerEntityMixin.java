15698f66c423ee5d25c5e1dc85f2cc0041699c16
charlyb01 <charly5bollinger@gmail?com>
2023-01-02 19:34:12 +0100

    Fix vanilla add experience function
    Bug reported to mojang.

---
+++ src/main/java/com/github/charlyb01/xpstorage/mixin/PlayerEntityMixin.java
@@ -0,0 +1,34 @@
+package com.github.charlyb01.xpstorage.mixin;
+
+import com.github.charlyb01.xpstorage.Utils;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.util.math.MathHelper;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(PlayerEntity.class)
+public abstract class PlayerEntityMixin {
+    @Shadow public int experienceLevel;
+    @Shadow public float experienceProgress;
+    @Shadow public int totalExperience;
+
+    @Shadow public abstract void addScore(int score);
+    @Shadow public abstract void addExperienceLevels(int levels);
+    @Shadow public abstract int getNextLevelExperience();
+
+    @Inject(method = "addExperience", at = @At("HEAD"), cancellable = true)
+    private void correctAddExperience(int experience, CallbackInfo ci) {
+        this.addScore(experience);
+
+        this.totalExperience = MathHelper.clamp(this.totalExperience + experience, 0, Integer.MAX_VALUE);
+        this.experienceLevel = 0;
+        this.addExperienceLevels(Utils.getLevelFromExperience(this.totalExperience));
+        int deltaExperience = this.totalExperience - Utils.getExperienceToLevel(this.experienceLevel);
+        this.experienceProgress = deltaExperience / (float)this.getNextLevelExperience();
+
+        ci.cancel();
+    }
+}
