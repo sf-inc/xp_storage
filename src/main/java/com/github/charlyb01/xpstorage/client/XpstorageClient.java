938735c59db001ddf6af905bf2628650ddb762b6
charlyb01 <charly5bollinger@gmail?com>
2023-01-03 19:31:54 +0100

    Update lang and bottles tooltips

--- src/main/java/com/github/charlyb01/xpstorage/client/XpstorageClient.java
+++ src/main/java/com/github/charlyb01/xpstorage/client/XpstorageClient.java
@@ -8,6 +8,7 @@ import net.fabricmc.api.Environment;
 import net.fabricmc.fabric.api.client.item.v1.ItemTooltipCallback;
 import net.minecraft.item.Items;
 import net.minecraft.text.Text;
+import net.minecraft.util.Formatting;
 
 @Environment(EnvType.CLIENT)
 public class XpstorageClient implements ClientModInitializer {
@@ -17,11 +18,11 @@ public class XpstorageClient implements ClientModInitializer {
             if (stack.getItem() == Items.EXPERIENCE_BOTTLE) {
                 int xpAmount = MyComponents.XP_COMPONENT.get(stack).getLevel();
                 if (xpAmount > 0) {
-                    if (ModConfig.get().cosmetic.bottleTooltip)
-                        lines.add(Text.translatable("item.xp_storage.experience_bottle.tooltip.strange"));
-                    lines.add(Text.translatable("item.xp_storage.experience_bottle.tooltip.amount", xpAmount));
+                    lines.add(Text.translatable("item.xp_storage.experience_bottle.tooltip.amount", xpAmount)
+                            .formatted(Formatting.GRAY));
                 } else if (ModConfig.get().bottles.enableBrewing && ModConfig.get().cosmetic.bottleTooltip) {
-                    lines.add(Text.translatable("item.xp_storage.experience_bottle.tooltip.classic"));
+                    lines.add(Text.translatable("item.xp_storage.experience_bottle.tooltip.classic")
+                            .formatted(Formatting.GRAY));
                 }
             }
         });
