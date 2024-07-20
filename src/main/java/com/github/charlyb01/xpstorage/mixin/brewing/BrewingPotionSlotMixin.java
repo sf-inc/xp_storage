48988c89c9e959477f806b0fa8ca5a585a37acb8
charlyb01 <charly5bollinger@gmail.com>
2024-07-20 20:52:08 +0200

    Use better mixin

--- src/main/java/com/github/charlyb01/xpstorage/mixin/brewing/BrewingPotionSlotMixin.java
+++ src/main/java/com/github/charlyb01/xpstorage/mixin/brewing/BrewingPotionSlotMixin.java
@@ -1,19 +1,16 @@
 package com.github.charlyb01.xpstorage.mixin.brewing;
 
 import com.github.charlyb01.xpstorage.config.ModConfig;
+import com.llamalad7.mixinextras.injector.ModifyReturnValue;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(targets = "net.minecraft.screen.BrewingStandScreenHandler$PotionSlot")
 public class BrewingPotionSlotMixin {
-    @Inject(method = "matches", at = @At("HEAD"), cancellable = true)
-    private static void makeXpBottlesMatches(ItemStack stack, CallbackInfoReturnable<Boolean> cir) {
-        if (ModConfig.get().bottles.enableBrewing && stack.isOf(Items.EXPERIENCE_BOTTLE)) {
-            cir.setReturnValue(true);
-        }
+    @ModifyReturnValue(method = "matches", at = @At("RETURN"))
+    private static boolean makeXpBottlesMatches(boolean original, ItemStack stack) {
+        return original || ModConfig.get().bottles.enableBrewing && stack.isOf(Items.EXPERIENCE_BOTTLE);
     }
 }
--- src/main/java/com/github/charlyb01/xpstorage/mixin/brewing/BrewingRecipeMixin.java
+++ src/main/java/com/github/charlyb01/xpstorage/mixin/brewing/BrewingRecipeMixin.java
@@ -2,19 +2,16 @@ package com.github.charlyb01.xpstorage.mixin.brewing;
 
 import com.github.charlyb01.xpstorage.XpBook;
 import com.github.charlyb01.xpstorage.config.ModConfig;
+import com.llamalad7.mixinextras.injector.ModifyReturnValue;
 import net.minecraft.item.ItemStack;
 import net.minecraft.recipe.BrewingRecipeRegistry;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(BrewingRecipeRegistry.class)
 public class BrewingRecipeMixin {
-    @Inject(method = "isValidIngredient", at = @At("HEAD"), cancellable = true)
-    private void registerXpBookIngredient(ItemStack stack, CallbackInfoReturnable<Boolean> cir) {
-        if (ModConfig.get().bottles.enableBrewing && stack.getItem() instanceof XpBook) {
-            cir.setReturnValue(true);
-        }
+    @ModifyReturnValue(method = "isValidIngredient", at = @At("RETURN"))
+    private boolean registerXpBookIngredient(boolean original, ItemStack stack) {
+        return original || ModConfig.get().bottles.enableBrewing && stack.getItem() instanceof XpBook;
     }
 }
