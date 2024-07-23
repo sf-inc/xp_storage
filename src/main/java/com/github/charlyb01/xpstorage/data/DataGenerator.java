a31a87a7d706e2a10e69db21263185e0449caa53
charlyb01 <charly5bollinger@gmail.com>
2024-07-23 14:33:14 +0200

    Replace data files with datagen
    To avoid mistakes like for 1.21 port

---
+++ src/main/java/com/github/charlyb01/xpstorage/data/DataGenerator.java
@@ -0,0 +1,13 @@
+package com.github.charlyb01.xpstorage.data;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class DataGenerator implements DataGeneratorEntrypoint {
+    @Override
+    public void onInitializeDataGenerator(FabricDataGenerator generator) {
+        FabricDataGenerator.Pack pack = generator.createPack();
+
+        pack.addProvider(RecipeProvider::new);
+    }
+}
---
+++ src/main/java/com/github/charlyb01/xpstorage/data/RecipeProvider.java
@@ -0,0 +1,65 @@
+package com.github.charlyb01.xpstorage.data;
+
+import com.github.charlyb01.xpstorage.Xpstorage;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
+import net.fabricmc.fabric.api.datagen.v1.provider.FabricRecipeProvider;
+import net.minecraft.data.server.recipe.RecipeExporter;
+import net.minecraft.data.server.recipe.ShapedRecipeJsonBuilder;
+import net.minecraft.item.Items;
+import net.minecraft.predicate.NumberRange;
+import net.minecraft.recipe.book.RecipeCategory;
+import net.minecraft.registry.RegistryWrapper;
+
+import java.util.concurrent.CompletableFuture;
+
+public class RecipeProvider extends FabricRecipeProvider {
+    public RecipeProvider(FabricDataOutput output, CompletableFuture<RegistryWrapper.WrapperLookup> registriesFuture) {
+        super(output, registriesFuture);
+    }
+
+    @Override
+    public void generate(RecipeExporter exporter) {
+        ShapedRecipeJsonBuilder.create(RecipeCategory.MISC, Xpstorage.CRYSTALLIZED_LAPIS, 2)
+                .pattern(" l ")
+                .pattern("lal")
+                .pattern(" l ")
+                .input('l', Items.LAPIS_LAZULI)
+                .input('a', Items.AMETHYST_SHARD)
+                .criterion(FabricRecipeProvider.hasItem(Items.LAPIS_LAZULI),
+                        FabricRecipeProvider.conditionsFromItem(NumberRange.IntRange.atLeast(4), Items.LAPIS_LAZULI))
+                .offerTo(exporter);
+
+        ShapedRecipeJsonBuilder.create(RecipeCategory.TOOLS, Xpstorage.xp_book1)
+                .pattern(" d ")
+                .pattern("cbc")
+                .pattern(" c ")
+                .input('c', Xpstorage.CRYSTALLIZED_LAPIS)
+                .input('b', Items.BOOK)
+                .input('d', Items.DIAMOND)
+                .criterion(FabricRecipeProvider.hasItem(Xpstorage.CRYSTALLIZED_LAPIS),
+                        FabricRecipeProvider.conditionsFromItem(Xpstorage.CRYSTALLIZED_LAPIS))
+                .offerTo(exporter);
+
+        ShapedRecipeJsonBuilder.create(RecipeCategory.TOOLS, Xpstorage.xp_book2)
+                .pattern(" n ")
+                .pattern("cbc")
+                .pattern(" c ")
+                .input('c', Xpstorage.CRYSTALLIZED_LAPIS)
+                .input('b', Xpstorage.xp_book1)
+                .input('n', Items.NETHERITE_INGOT)
+                .criterion(FabricRecipeProvider.hasItem(Xpstorage.xp_book1),
+                        FabricRecipeProvider.conditionsFromItem(Xpstorage.xp_book1))
+                .offerTo(exporter);
+
+        ShapedRecipeJsonBuilder.create(RecipeCategory.TOOLS, Xpstorage.xp_book3)
+                .pattern(" n ")
+                .pattern("cbc")
+                .pattern(" c ")
+                .input('c', Xpstorage.CRYSTALLIZED_LAPIS)
+                .input('b', Xpstorage.xp_book2)
+                .input('n', Items.NETHER_STAR)
+                .criterion(FabricRecipeProvider.hasItem(Xpstorage.xp_book2),
+                        FabricRecipeProvider.conditionsFromItem(Xpstorage.xp_book2))
+                .offerTo(exporter);
+    }
+}
